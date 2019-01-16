10cfb41d15f96d85e3e58a11a751f3acca967f31
wei.mao <wei.mao@pintec.com>
2019-01-17 01:06:21 +0800

    ci

---
+++ sdk/src/main/java/com/cdts/sdk/extension/Date+Ex.kt
@@ -0,0 +1,9 @@
+package com.cdts.sdk.extension
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Date.format(format: String = "yyyy-MM-dd HH:mm:ss"): String {
+    val formatter = SimpleDateFormat(format)
+    return formatter.format(this)
+}
---
+++ sdk/src/test/java/com/cdts/sdk/extension/DateExTests.kt
@@ -0,0 +1,13 @@
+package com.cdts.sdk.extension
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.*
+
+class DateExTests {
+    @Test
+    fun testDate() {
+        val date = Date().format()
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
