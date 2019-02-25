4d3b5ee6ff0e9b43fcc439dee221147004bb6f55
wei.mao <wei.mao@pintec.com>
2019-02-25 10:09:59 +0800

    umeng

--- app/src/main/java/com/cdts/demo/libs/UMengLib.kt
+++ app/src/main/java/com/cdts/demo/libs/UMengLib.kt
@@ -3,11 +3,13 @@ package com.cdts.demo.libs
 import android.content.Context
 import com.cdts.demo.config.GlobalConfig
 import com.cdts.demo.config.UMengConfig
+import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
 
 object UMengLib {
     fun setup(context: Context) {
         UMConfigure.init(context, UMengConfig.appKey, GlobalConfig.channel, UMConfigure.DEVICE_TYPE_PHONE, "")
+        MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
         if (!GlobalConfig.isRelease) {
             UMConfigure.setLogEnabled(true)
         }
